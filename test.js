07d9737df10b5f01a21bc080c235d0e68905eb72
Mathias Buus <mathiasbuus@gmail.com>
2022-09-12 10:36:57 +0200

    failing test

--- test.js
+++ test.js
@@ -24,3 +24,45 @@ tape('throttled by eachother', function (t) {
     t.end()
   }, 200)
 })
+
+tape('does not premature destroy', function (t) {
+  const r = new Readable()
+
+  const [a, b] = tee(r)
+
+  r.push('a')
+  r.push('b')
+  r.push('c')
+  r.push(null)
+
+  setTimeout(() => {
+    const aSeen = []
+    const bSeen = []
+
+    a.on('data', function (data) {
+      aSeen.push(data)
+    })
+    a.on('end', function () {
+      aSeen.push(null)
+    })
+
+    b.on('data', function (data) {
+      bSeen.push(data)
+    })
+    b.on('end', function () {
+      bSeen.push(null)
+    })
+
+    let missing = 2
+    a.on('close', onclose)
+    b.on('close', onclose)
+
+    function onclose () {
+      if (--missing === 0) {
+        t.same(aSeen, ['a', 'b', 'c', null])
+        t.same(bSeen, ['a', 'b', 'c', null])
+        t.end()
+      }
+    }
+  }, 200)
+})
